f190ce24e7afe95625a26392140b0f0e2a5bfedd
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2018-08-10 22:57:32 +0100

    Datastore entity - #8

---
+++ acropolis-persistence/src/main/kotlin/org/ephyra/acropolis/persistence/api/entity/DatastoreEntity.kt
@@ -0,0 +1,21 @@
+package org.ephyra.acropolis.persistence.api.entity
+
+import javax.persistence.*
+
+/**
+ * Entity to model simple data stores. From local hard drives to remote buckets.
+ */
+@Entity
+class DatastoreEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Int = 0
+
+    @NotNull
+    var name: String = ""
+
+    var description: String? = null
+
+    @ManyToOne(optional=false)
+    var project: ProjectEntity? = null
+}
