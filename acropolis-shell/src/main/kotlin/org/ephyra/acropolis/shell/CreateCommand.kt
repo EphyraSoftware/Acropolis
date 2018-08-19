b2d6634b5d74b20e9f6581ea28ca9051033ae6ed
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2018-08-19 16:40:09 +0100

    Shell - Create Datastore - #8

--- acropolis-shell/src/main/kotlin/org/ephyra/acropolis/shell/CreateCommand.kt
+++ acropolis-shell/src/main/kotlin/org/ephyra/acropolis/shell/CreateCommand.kt
@@ -1,6 +1,7 @@
 package org.ephyra.acropolis.shell
 
 import org.ephyra.acropolis.service.api.IApplicationSoftwareService
+import org.ephyra.acropolis.service.api.IDatastoreService
 import org.ephyra.acropolis.service.api.IProjectService
 import org.ephyra.acropolis.service.api.ISystemSoftwareService
 import org.springframework.beans.factory.annotation.Autowired
@@ -21,12 +22,16 @@ class CreateCommand {
     @Autowired
     private lateinit var systemSoftwareService: ISystemSoftwareService
 
+    @Autowired
+    private lateinit var datastoreService: IDatastoreService
+
     @ShellMethod("Create an item")
     fun create(itemType: String, name: String) {
         when (itemType) {
             "project" -> createProject(name)
             "application-software" -> createApplicationSoftware(name)
             "system-software" -> createSystemSoftware(name)
+            "datastore" -> createDatastore(name)
             else -> println("Don't know how to create an item with type [$itemType]")
         }
     }
@@ -45,6 +50,16 @@ class CreateCommand {
         val project = appState.currentProject
         if (project != null) {
             applicationSoftwareService.create(project.id, name)
+        }
+        else {
+            println("No project selected")
+        }
+    }
+
+    private fun createDatastore(name: String) {
+        val project = appState.currentProject
+        if (project != null) {
+            datastoreService.create(project.id, name)
         }
         else {
             println("No project selected")
