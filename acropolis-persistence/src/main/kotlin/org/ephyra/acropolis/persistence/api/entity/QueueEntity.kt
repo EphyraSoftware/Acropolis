647e1925e285e1be890a032b4c3deef75c2e56ac
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2018-08-22 20:19:38 +0100

    #13 Add name to QueueEntity

--- acropolis-persistence/src/main/kotlin/org/ephyra/acropolis/persistence/api/entity/QueueEntity.kt
+++ acropolis-persistence/src/main/kotlin/org/ephyra/acropolis/persistence/api/entity/QueueEntity.kt
@@ -1,12 +1,17 @@
 package org.ephyra.acropolis.persistence.api.entity
 
 import org.ephyra.acropolis.persistence.api.ConnectionType
+import javax.persistence.Column
 import javax.persistence.Entity
 
 @Entity
-class QueueEntity @JvmOverloads constructor (
-        desc: String? = null
+class QueueEntity @JvmOverloads constructor(
+    @Column(nullable = false)
+    var name: String,
+
+    desc: String? = null
 ) : SystemSoftwareSpecializationEntity(desc) {
+
     override fun getConnectionType(): Int {
         return ConnectionType.QUEUE.type
     }
