ea90ddc5492f59978a444e8d2ed0f03db9215097
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-09-04 23:49:26 +0100

    #35 Pick testing libraries and create a very simple test

---
+++ acropolis-service/src/test/kotlin/org/ephyra/acropolis/service/ApplicationSoftwareServiceTest.kt
@@ -0,0 +1,42 @@
+package org.ephyra.acropolis.service
+
+import io.kotlintest.Description
+import io.kotlintest.Spec
+import io.kotlintest.extensions.TestListener
+import io.kotlintest.specs.StringSpec
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import org.ephyra.acropolis.persistence.api.persistence.ApplicationSoftwarePersistence
+import org.ephyra.acropolis.service.api.IApplicationSoftwareService
+import org.ephyra.acropolis.service.impl.ApplicationSoftwareService
+
+class ApplicationSoftwareServiceTest : StringSpec() {
+    @MockK(relaxUnitFun = true)
+    lateinit var persistence: ApplicationSoftwarePersistence
+
+    @InjectMockKs
+    var testClass: IApplicationSoftwareService = ApplicationSoftwareService()
+
+    override fun listeners(): List<TestListener> = listOf(MockKInitializer(this))
+
+    init {
+        "Create a new application software" {
+            testClass.create(1, "my-app")
+            verify { persistence.create(application = any()) }
+        }
+    }
+}
+
+class MockKInitializer (
+        private var initMe: ApplicationSoftwareServiceTest
+) : TestListener {
+
+    override fun beforeSpec(description: Description, spec: Spec) {
+        super.beforeSpec(description, spec)
+
+        MockKAnnotations.init(initMe)
+    }
+}
