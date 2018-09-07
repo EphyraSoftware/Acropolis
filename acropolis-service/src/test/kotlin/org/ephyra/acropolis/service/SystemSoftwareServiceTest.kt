008bc8d2636ae16326125de2572f244827a2c270
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-09-07 22:03:46 +0100

    #35 Test the system software service

--- acropolis-service/src/test/kotlin/org/ephyra/acropolis/service/SystemSoftwareServiceTest.kt
+++ acropolis-service/src/test/kotlin/org/ephyra/acropolis/service/SystemSoftwareServiceTest.kt
@@ -1,78 +1,79 @@
 package org.ephyra.acropolis.service
 
-import io.kotlintest.*
 import io.kotlintest.extensions.TestListener
 import io.kotlintest.matchers.string.shouldStartWith
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
+import io.kotlintest.shouldThrowAny
 import io.kotlintest.specs.StringSpec
-import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import io.mockk.verify
-import org.ephyra.acropolis.persistence.api.entity.ApplicationSoftwareEntity
 import org.ephyra.acropolis.persistence.api.persistence.ApplicationSoftwarePersistence
+import org.ephyra.acropolis.persistence.api.persistence.SystemSoftwarePersistence
 import org.ephyra.acropolis.service.api.IApplicationSoftwareService
+import org.ephyra.acropolis.service.api.ISystemSoftwareService
 import org.ephyra.acropolis.service.impl.ApplicationSoftwareService
+import org.ephyra.acropolis.service.impl.SystemSoftwareService
 
 /**
- * Tests for the application-software service.
+ * Tests for the system software service
  */
-class ApplicationSoftwareServiceTest : StringSpec() {
+class SystemSoftwareServiceTest : StringSpec() {
     @MockK(relaxUnitFun = true)
-    lateinit var persistence: ApplicationSoftwarePersistence
+    lateinit var persistence: SystemSoftwarePersistence
 
     @InjectMockKs
-    var testClass: IApplicationSoftwareService = ApplicationSoftwareService()
+    var testClass: ISystemSoftwareService = SystemSoftwareService()
 
     override fun listeners(): List<TestListener> = listOf(MockKInitializer(this))
 
     init {
-        "Create a new application software" {
-            testClass.create(1, "my-app")
-            verify { persistence.create(application = any()) }
+        "Create a new system software" {
+            testClass.create(1, "my-service")
+            verify { persistence.create(systemSoftware = any()) }
         }
 
-        "Create a new application software, fails to save" {
-            every { persistence.create(application = any()) } throws Exception("failed to save")
+        "Create a new system software, fails to save" {
+            every { persistence.create(systemSoftware = any()) } throws Exception("failed to save")
 
             val exception = shouldThrowAny {
-                testClass.create(1, "my-app")
+                testClass.create(1, "my-service")
             }
             exception.message.shouldStartWith("failed to save")
         }
 
-        "Find application software by name, not found" {
+        "Find system software by name, not found" {
             val name = "mysoft"
             val projectId: Long = 1
 
             every { persistence.findByName(name, projectId) } returns null
 
-            testClass.find(name, projectId).shouldBe(null)
+            testClass.get(name, projectId).shouldBe(null)
 
             verify { persistence.findByName(name, projectId) }
         }
 
-        "Find application software by name" {
+        "Find system software by name" {
             val name = "mysoft"
             val projectId: Long = 1
 
             every { persistence.findByName(name, projectId) } returns mockk()
 
-            testClass.find(name, projectId).shouldNotBe(null)
+            testClass.get(name, projectId).shouldNotBe(null)
 
             verify { persistence.findByName(name, projectId) }
         }
 
-        "Find application software by name, fails to lookup" {
+        "Find system software by name, fails to lookup" {
             every { persistence.findByName(name = any(), projectId = any()) } throws Exception("failed to lookup")
 
             val exception = shouldThrowAny {
-                testClass.find("", 1)
+                testClass.get("", 1)
             }
             exception.message.shouldStartWith("failed to lookup")
         }
     }
 }
-
-
