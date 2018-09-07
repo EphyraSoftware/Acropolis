cc97c7764b98ca0db5b1aeed959f4e837694ccdd
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-09-07 20:42:29 +0100

    #35 Add tests for application software service

--- acropolis-service/src/test/kotlin/org/ephyra/acropolis/service/ApplicationSoftwareServiceTest.kt
+++ acropolis-service/src/test/kotlin/org/ephyra/acropolis/service/ApplicationSoftwareServiceTest.kt
@@ -1,14 +1,16 @@
 package org.ephyra.acropolis.service
 
-import io.kotlintest.Description
-import io.kotlintest.Spec
+import io.kotlintest.*
 import io.kotlintest.extensions.TestListener
+import io.kotlintest.matchers.string.shouldStartWith
 import io.kotlintest.specs.StringSpec
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import io.mockk.verify
+import org.ephyra.acropolis.persistence.api.entity.ApplicationSoftwareEntity
 import org.ephyra.acropolis.persistence.api.persistence.ApplicationSoftwarePersistence
 import org.ephyra.acropolis.service.api.IApplicationSoftwareService
 import org.ephyra.acropolis.service.impl.ApplicationSoftwareService
@@ -29,6 +31,46 @@ class ApplicationSoftwareServiceTest : StringSpec() {
         "Create a new application software" {
             testClass.create(1, "my-app")
             verify { persistence.create(application = any()) }
+        }
+
+        "Create a new application software, fails to save" {
+            every { persistence.create(application = any()) } throws Exception("failed to save")
+
+            val exception = shouldThrowAny {
+                testClass.create(1, "my-app")
+            }
+            exception.message.shouldStartWith("failed to save")
+        }
+
+        "Find application software by name, not found" {
+            val name = "mysoft"
+            val projectId: Long = 1
+
+            every { persistence.findByName(name, projectId) } returns null
+
+            testClass.find(name, projectId).shouldBe(null)
+
+            verify { persistence.findByName(name, projectId) }
+        }
+
+        "Find application software by name" {
+            val name = "mysoft"
+            val projectId: Long = 1
+
+            every { persistence.findByName(name, projectId) } returns mockk()
+
+            testClass.find(name, projectId).shouldNotBe(null)
+
+            verify { persistence.findByName(name, projectId) }
+        }
+
+        "Find application software by name, fails to lookup" {
+            every { persistence.findByName(name = any(), projectId = any()) } throws Exception("failed to lookup")
+
+            var exception = shouldThrowAny {
+                testClass.find("", 1)
+            }
+            exception.message.shouldStartWith("failed to lookup")
         }
     }
 }
