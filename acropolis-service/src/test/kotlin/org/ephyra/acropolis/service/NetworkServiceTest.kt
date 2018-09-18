e272d5195393842c599a3d7a374902d2116f2c00
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-09-19 00:32:46 +0100

    #35 Add tests for link datastore to network

--- acropolis-service/src/test/kotlin/org/ephyra/acropolis/service/NetworkServiceTest.kt
+++ acropolis-service/src/test/kotlin/org/ephyra/acropolis/service/NetworkServiceTest.kt
@@ -11,6 +11,9 @@ import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import io.mockk.verify
+import org.ephyra.acropolis.persistence.api.entity.DatastoreEntity
+import org.ephyra.acropolis.persistence.api.entity.GroupingEntity
+import org.ephyra.acropolis.persistence.api.entity.NetworkEntity
 import org.ephyra.acropolis.persistence.api.persistence.GroupingPersistence
 import org.ephyra.acropolis.persistence.api.persistence.NetworkPersistence
 import org.ephyra.acropolis.service.api.*
@@ -49,6 +52,33 @@ class NetworkServiceTest : StringSpec() {
             fixture.givenNetworkFoundByName()
             fixture.whenNetworkLookedUpByNameThenNetworkFound()
         }
+
+        "Link datastore with network not found, throws exception" {
+            fixture.givenNetworkNotFoundById()
+            fixture.whenDatastoreLinkedThenExceptionThrownForMissingNetwork()
+        }
+
+        "Link datastore with datastore not found, throws exception" {
+            fixture.givenNetworkFoundById()
+            fixture.givenDatastoreNotFoundById()
+            fixture.whenDatastoreLinkedThenExceptionThrownForMissingDatastore()
+        }
+
+        "Link datastore to network with no existing links" {
+            fixture.givenNetworkWithNoGroupingIsFoundById()
+            fixture.givenDatastoreFoundByName()
+            fixture.whenDatastoreLinked()
+            fixture.thenNewGroupingCreated()
+            fixture.thenNewGroupingLinkedToNetwork()
+        }
+
+        "Link datastore to network with existing links" {
+            fixture.givenNetworkWithExistingGroupingIsFoundById()
+            fixture.givenDatastoreFoundByName()
+            fixture.whenDatastoreLinked()
+            fixture.thenTheGroupingIsUpdated()
+            fixture.thenDatastoreLinkedToNetwork()
+        }
     }
 }
 
@@ -79,7 +109,14 @@ internal class NetworkServiceTestFixture {
 
     private val testProjectName = "testProjectName"
     private val testProjectId: Long = 1
+
     private val testNetworkName = "testNetworkName"
+    private val testNetworkId: Long = 2
+
+    private val testDatastoreName = "testDatastoreName"
+
+    private var newGrouping: GroupingEntity? = null
+    private var datastoreList: MutableList<DatastoreEntity> = ArrayList()
 
     fun givenProjectNotFound() {
         every { projectService.get(testProjectName) } returns null
@@ -118,6 +155,77 @@ internal class NetworkServiceTestFixture {
 
     fun whenNetworkLookedUpByNameThenNetworkFound() {
         testClass.get(testNetworkName, testProjectId).shouldNotBe(null)
+    }
+
+    fun givenNetworkNotFoundById() {
+        every { persistence.find(testNetworkId, testProjectId) } returns null
+    }
+
+    fun whenDatastoreLinkedThenExceptionThrownForMissingNetwork() {
+        val exception = shouldThrow<IllegalStateException> {
+            testClass.linkDatastore(testNetworkId, testDatastoreName, testProjectId)
+        }
+        exception.message.shouldBe("Cannot link datastore to network because network with id [$testNetworkId] was not found")
+    }
+
+    fun givenNetworkFoundById() {
+        every { persistence.find(testNetworkId, testProjectId) } returns mockk()
+    }
+
+    fun givenDatastoreNotFoundById() {
+        every { datastoreService.get(testDatastoreName, testProjectId) } returns null
+    }
+
+    fun whenDatastoreLinkedThenExceptionThrownForMissingDatastore() {
+        val exception = shouldThrow<IllegalStateException> {
+            testClass.linkDatastore(testNetworkId, testDatastoreName, testProjectId)
+        }
+        exception.message.shouldBe("Cannot link datastore to network because datastore with name [$testDatastoreName] was not found")
+    }
+
+    fun givenNetworkWithNoGroupingIsFoundById() {
+        val network: NetworkEntity = mockk()
+        every { network.groupingEntity } returns null
+        every { network.groupingEntity = any() } propertyType GroupingEntity::class answers {
+            newGrouping = value
+        }
+        every { persistence.find(testNetworkId, testProjectId) } returns network
+    }
+
+    fun givenDatastoreFoundByName() {
+        every { datastoreService.get(testDatastoreName, testProjectId) } returns mockk()
+    }
+
+    fun whenDatastoreLinked() {
+        testClass.linkDatastore(testNetworkId, testDatastoreName, testProjectId)
+    }
+
+    fun thenNewGroupingCreated() {
+        verify { groupingPersistence.create(entity = any()) }
+    }
+
+    fun thenNewGroupingLinkedToNetwork() {
+        // Means that the grouping entity has been assigned to the groupingEntity property on the network
+        newGrouping.shouldNotBe(null)
+        verify { persistence.updateGrouping(entity = any()) }
+    }
+
+    fun givenNetworkWithExistingGroupingIsFoundById() {
+        val network: NetworkEntity = mockk()
+
+        val grouping: GroupingEntity = mockk()
+        every { grouping.datastoreList } returns datastoreList
+
+        every { network.groupingEntity } returns grouping
+        every { persistence.find(testNetworkId, testProjectId) } returns network
+    }
+
+    fun thenTheGroupingIsUpdated() {
+        verify { groupingPersistence.update(entity = any()) }
+    }
+
+    fun thenDatastoreLinkedToNetwork() {
+        datastoreList.size.shouldBe(1)
     }
 }
 
