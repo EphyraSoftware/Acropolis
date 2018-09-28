106d31b12578a900ec0135f9e84ef91f754eb010
ThetaSinner <greatdjonfire@hotmail.co.uk>
2018-09-28 22:01:46 +0100

    #48 Add and test specialization field for system software

--- acropolis-external/src/main/kotlin/org/ephyra/acropolis/external/model/SystemSoftware.kt
+++ acropolis-external/src/main/kotlin/org/ephyra/acropolis/external/model/SystemSoftware.kt
@@ -6,5 +6,6 @@ package org.ephyra.acropolis.external.model
 data class SystemSoftware (
         var name: String,
         var description: String,
+        var specialization: String?,
         var talks_to: List<String>?
 )
--- acropolis-external/src/test/kotlin/org/ephyra/acropolis/external/YamlHelperTest.kt
+++ acropolis-external/src/test/kotlin/org/ephyra/acropolis/external/YamlHelperTest.kt
@@ -21,7 +21,7 @@ class YamlHelperTest : StringSpec() {
                     - name: IceWeasel
                       description: 'Browser which the user interacts with'
                       talks_to:
-                        - system.RenderQueue
+                        - system.RenderProxy
                     - name: PrettyChart
                       description: 'Web app for rendering charts in a browser'
                       talks_to:
@@ -29,10 +29,21 @@ class YamlHelperTest : StringSpec() {
                   systems:
                     - name: ChartRenderer
                       description: 'Engine for rendering charts to offload work from the browser'
-                      talks_to:
-                        - system.RenderQueue
                     - name: RenderQueue
                       description: Queue for render jobs
+                      specialization: Queue
+                      talks_to:
+                        - system.ChartRenderer
+                    - name: RenderProxy
+                      description: Reverse Proxy for the render service
+                      specialization: ReverseProxy
+                      talks_to:
+                        - system.RenderLoadBalancer
+                    - name: RenderLoadBalancer
+                      description: Load balancer for the render service
+                      specialization: LoadBalancer
+                      talks_to:
+                        - system.RenderQueue
             """.trimIndent())
 
             loaded.shouldNotBe(null)
@@ -43,20 +54,32 @@ class YamlHelperTest : StringSpec() {
             loaded?.software?.applications?.get(0)?.name.shouldBe("IceWeasel")
             loaded?.software?.applications?.get(1)?.name.shouldBe("PrettyChart")
 
-            loaded?.software?.systems?.size.shouldBe(2)
+            loaded?.software?.systems?.size.shouldBe(4)
             loaded?.software?.systems?.get(0)?.name.shouldBe("ChartRenderer")
             loaded?.software?.systems?.get(1)?.name.shouldBe("RenderQueue")
+            loaded?.software?.systems?.get(2)?.name.shouldBe("RenderProxy")
+            loaded?.software?.systems?.get(3)?.name.shouldBe("RenderLoadBalancer")
 
             loaded?.software?.applications?.get(0)?.talks_to?.size.shouldBe(1)
-            loaded?.software?.applications?.get(0)?.talks_to?.get(0).shouldBe("system.RenderQueue")
+            loaded?.software?.applications?.get(0)?.talks_to?.get(0).shouldBe("system.RenderProxy")
 
             loaded?.software?.applications?.get(1)?.talks_to?.size.shouldBe(1)
             loaded?.software?.applications?.get(1)?.talks_to?.get(0).shouldBe("application.IceWeasel")
 
-            loaded?.software?.systems?.get(0)?.talks_to?.size.shouldBe(1)
-            loaded?.software?.systems?.get(0)?.talks_to?.get(0).shouldBe("system.RenderQueue")
+            loaded?.software?.systems?.get(0)?.talks_to.shouldBe(null)
+            loaded?.software?.systems?.get(0)?.specialization.shouldBe(null)
 
-            loaded?.software?.systems?.get(1)?.talks_to.shouldBe(null)
+            loaded?.software?.systems?.get(1)?.talks_to?.size.shouldBe(1)
+            loaded?.software?.systems?.get(1)?.talks_to?.get(0).shouldBe("system.ChartRenderer")
+            loaded?.software?.systems?.get(1)?.specialization.shouldBe("Queue")
+
+            loaded?.software?.systems?.get(2)?.talks_to?.size.shouldBe(1)
+            loaded?.software?.systems?.get(2)?.talks_to?.get(0).shouldBe("system.RenderLoadBalancer")
+            loaded?.software?.systems?.get(2)?.specialization.shouldBe("ReverseProxy")
+
+            loaded?.software?.systems?.get(3)?.talks_to?.size.shouldBe(1)
+            loaded?.software?.systems?.get(3)?.talks_to?.get(0).shouldBe("system.RenderQueue")
+            loaded?.software?.systems?.get(3)?.specialization.shouldBe("LoadBalancer")
         }
     }
 }
