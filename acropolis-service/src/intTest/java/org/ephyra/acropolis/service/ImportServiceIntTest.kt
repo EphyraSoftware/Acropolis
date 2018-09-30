62ee65a6c666f6462fa8ee57cbedffbb55d49cf1
ThetaSinner <greatdjonfire@hotmail.co.uk>
2018-09-30 23:31:21 +0100

    #51 First stab at configuring integration tests

---
+++ acropolis-service/src/intTest/java/org/ephyra/acropolis/service/ImportServiceIntTest.kt
@@ -0,0 +1,14 @@
+package org.ephyra.acropolis.service
+
+import io.kotlintest.specs.StringSpec
+
+/**
+ * Integration test for the import service
+ */
+class ImportServiceIntTest : StringSpec() {
+    init {
+        "can run integration tests" {
+            println("Hello, IntTest")
+        }
+    }
+}
