82791cca3541cfaaa48b655f57a4822e21307104
ThetaSinner <greatdjonfire@hotmail.co.uk>
2018-10-01 23:56:51 +0100

    #51 Starting configuring spring test for integration testing

--- acropolis-service/src/intTest/kotlin/org/ephyra/acropolis/service/ImportServiceIntTest.kt
+++ acropolis-service/src/intTest/kotlin/org/ephyra/acropolis/service/ImportServiceIntTest.kt
@@ -1,14 +1,27 @@
 package org.ephyra.acropolis.service
 
-import io.kotlintest.specs.StringSpec
+import org.ephyra.acropolis.service.api.IImportService
+import org.ephyra.acropolis.service.config.ServiceConfiguration
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
 /**
  * Integration test for the import service
  */
-class ImportServiceIntTest : StringSpec() {
-    init {
-        "can run integration tests" {
-            println("Hello, IntTest")
-        }
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(classes = [ServiceConfiguration::class])
+class ImportServiceIntTest {
+    @Autowired
+    lateinit var importService: IImportService
+
+    @Test
+    @DisplayName("Test that YAML can be used to import a project")
+    fun importProjectFromYaml() {
+        assertNotEquals(null, importService)
     }
 }
