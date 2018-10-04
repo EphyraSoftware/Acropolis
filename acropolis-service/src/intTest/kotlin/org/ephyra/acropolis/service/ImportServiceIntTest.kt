746709b089bced96e6c3516fe244b55bfe059250
ThetaSinner <greatdjonfire@hotmail.co.uk>
2018-10-05 00:57:49 +0100

    #51 Fix the integration test problem with entityManagerFactory not defined
    turns out that this is automatically defined as part of the normal spring boot startup, but not in test so some config is needed!

--- acropolis-service/src/intTest/kotlin/org/ephyra/acropolis/service/ImportServiceIntTest.kt
+++ acropolis-service/src/intTest/kotlin/org/ephyra/acropolis/service/ImportServiceIntTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
  */
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(classes = [ServiceConfiguration::class])
+@DataJpaTest
 class ImportServiceIntTest {
     @Autowired
     lateinit var importService: IImportService
