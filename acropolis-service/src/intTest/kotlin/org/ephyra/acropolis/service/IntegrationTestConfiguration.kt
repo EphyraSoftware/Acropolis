49c108bd1e059781f2f35f046ececc200fec1be4
ThetaSinner <greatdjonfire@hotmail.co.uk>
2018-10-11 00:18:31 +0100

    #51 Attempt a fix for the it tests
    went back to the initial setup and added changes in bit by bit using the a diff between the two. Still don't know what the problem was but it looks like it might be something to do with application.properties existing

--- acropolis-service/src/intTest/kotlin/org/ephyra/acropolis/service/IntegrationTestConfiguration.kt
+++ acropolis-service/src/intTest/kotlin/org/ephyra/acropolis/service/IntegrationTestConfiguration.kt
@@ -1,6 +1,5 @@
 package org.ephyra.acropolis.service
 
-import com.google.common.base.Preconditions
 import org.ephyra.acropolis.service.config.ServiceConfiguration
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
@@ -42,18 +41,19 @@ class IntegrationTestConfiguration {
     fun dataSource(): DataSource {
         val dataSource = DriverManagerDataSource()
 
-        dataSource.setDriverClassName(environment.getRequiredProperty("spring.datasource.driverClassName"))
-        dataSource.url = Preconditions.checkNotNull(environment.getRequiredProperty("spring.datasource.url"))
-        dataSource.username = Preconditions.checkNotNull(environment.getRequiredProperty("spring.datasource.username"))
-        dataSource.password = Preconditions.checkNotNull(environment.getRequiredProperty("spring.datasource.password"))
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver")
+        dataSource.url = "jdbc:mysql://localhost:3306/acropolisdev"
+        dataSource.username = "root"
+        dataSource.password = "root"
 
         return dataSource
     }
 
     fun additionalProperties(): Properties {
         val hibernateProperties = Properties()
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", environment.getRequiredProperty("spring.jpa.hibernate.ddl-auto"))
-        hibernateProperties.setProperty("hibernate.dialect", environment.getRequiredProperty("spring.jpa.hibernate.dialect"))
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create")
+        // TODO (Gregory Jensen 11/10/2018) This should be the MySQL8 dialect but that isn't available in the version of hibernate distributed with spring-data-jpa
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect")
         return hibernateProperties
     }
 }
