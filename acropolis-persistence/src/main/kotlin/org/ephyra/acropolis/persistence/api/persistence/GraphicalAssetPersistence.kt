844acdb59ad84ecda3c2af971b8bd0ec94b26c01
ThetaSinner <greatdjonfire@hotmail.co.uk>
2018-10-23 12:27:19 +0100

    #57 Add missing doc comment in persistence
    the persistence doc comments reveal implementation details, should all be looked at

--- acropolis-persistence/src/main/kotlin/org/ephyra/acropolis/persistence/api/persistence/GraphicalAssetPersistence.kt
+++ acropolis-persistence/src/main/kotlin/org/ephyra/acropolis/persistence/api/persistence/GraphicalAssetPersistence.kt
@@ -5,6 +5,9 @@ import org.ephyra.acropolis.persistence.impl.GraphicalAssetRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
+/**
+ * Wrapper around GraphicalAssetRepository operations.
+ */
 @Component
 class GraphicalAssetPersistence {
     @Autowired
